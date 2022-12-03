7e9b521c64deb056698b5bc8da259e3a8f92be4a
SarvinSaravi <sarvin2000@gmail.com>
2022-12-03 14:39:46 +0330

    git ignore video files + module1 is ok

--- code/module 1.py
+++ code/module 1.py
@@ -1,10 +1,11 @@
 import os
 import moviepy.editor as movpy
+import time
 
+start_time = time.time()
 
-start_dir = os.getcwd()
-# start_dir = '/movie/'
-
+# start_dir = os.getcwd()
+start_dir = 'D:\\Python\\dubbed_project\\code'
 
 # def convert_to_mp4(mkv_file):
 #     name, ext = os.path.splitext(mkv_file)
@@ -24,7 +25,10 @@ for path, folder, files in os.walk(start_dir):
             print("Found file: %s" % file)
             # convert_to_mp4(os.path.join(start_dir, file))
 
-            clip = movpy.VideoFileClip("Frnds-S03E09-720p_FiLMin.mkv")  # Reading .mkv file
-            clip.write_videofile("video.mp4", codec="libx264", audio_codec="aac")  # Writing .mp4 file
+            clip = movpy.VideoFileClip(file.title())  # Reading .mkv file
+            clip.write_videofile("video2.mp4", codec="libx264", audio_codec="aac")  # Writing .mp4 file
         else:
             pass
+
+print()
+print("--- %s seconds ---" % (time.time() - start_time))
