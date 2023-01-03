102f3b2df160962b090f163d529fdc7879d6f1d7
SarvinSaravi <sarvin2000@gmail.com>
2023-01-03 22:16:50 +0330

    change

--- stt_module/speech_extracter.py
+++ stt_module/speech_extracter.py
@@ -5,5 +5,3 @@ def extract_audio_from_video():
     clip = mp.VideoFileClip(r"Frnds-S03E09-720p_FiLMin.mkv")  # specify the correct file path to your video file
     clip.audio.write_audiofile(r"audio.wav")  # this the name of the converted audio file
 
-
-extract_audio_from_video()
