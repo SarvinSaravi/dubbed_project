9a8ceb51ae627012f7bdb1c1e6a834d2bffb557d
mbidkham <42229457+mbidkham@users.noreply.github.com>
2023-02-09 21:55:28 +0330

    transform (#3)

--- stt_module/main.py
+++ stt_module/main.py
@@ -1,13 +1,14 @@
 from stt_module.MP4_converter import mkv_to_mp4_converter
 from stt_module.speech_extracter import extract_audio_from_video
 from stt_module.text_converter import speech_to_text_converter
+from stt_module.transformer import transform
 
 
 def main():
     mkv_to_mp4_converter()
     extract_audio_from_video()
     speech_to_text_converter()
-
+    transform()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
